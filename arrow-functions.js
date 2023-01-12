e871d1ff2904e89c44df9813bb2f369c24a5bf52
RatiuCristian1 <lafurnica2008@yahoo.com>
2023-01-12 10:40:20 +0000

    Adding new code for the arrow-function

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,42 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     // Code Block
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum)
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    // Code Block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5)
+console.log(sum)
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b
+let sum2 = addTwoNumbers(5, 75)
+console.log(sum2) 
+
 // Implicit Returns
 
+const saySomething = message => console.log(message)
+saySomething('Hello!!')
+
 // Returning Multiple Lines
+
+const sayHello = () => console.log('Hey There')
+sayHello()
+
+const returnMultipleLines = () => (
+    `<p>
+        Multiple Line code
+    </p>`
+)
+console.log(returnMultipleLines())
